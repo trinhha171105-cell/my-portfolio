a9fd1601a230a10711ff526f03faf150e2108ea0
trinhha171105-cell <trinhha171105@gmail.com>
2026-03-28 22:36:00 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio của Hà Tuyết Trinh đã load xong!');
+
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+});
